8af1156b68b326b6c11ce9641630401bbe2703e2
XxSNIPERxX961 <kserhane2@gmail.com>
2025-05-30 04:57:02 +0300

    Add files via upload

---
+++ BMW_i8/__resource.lua
@@ -0,0 +1,24 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files {
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+    'handling.meta',
+    'sfx/dlc_rocket/rocket.awc',
+    'sfx/dlc_rocket/rocket_npc.awc',
+    'audioconfig/rocket_game.dat151.rel',
+    'audioconfig/rocket_sounds.dat54.rel',
+    'audioconfig/rocket_amp.dat10.rel'
+  
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_rocket'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/rocket_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/rocket_sounds.dat'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/rocket_amp.dat'
+
