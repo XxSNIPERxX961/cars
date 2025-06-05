114ed7c242ece7314d9aaaed0c3065c6a8c818a6
XxSNIPERxX961 <kserhane2@gmail.com>
2025-06-05 21:39:14 +0300

    Update __resource.lua

--- DOD_venomcharger/__resource.lua
+++ DOD_venomcharger/__resource.lua
@@ -17,7 +17,10 @@ data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 data_file 'CARCOLS_FILE' 'carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
 data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'   -- Not Required
-
+data_file "AUDIO_SYNTHDATA" "audioconfig/dchallengerhellcat_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/dchallengerhellcat_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/dchallengerhellcat_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_dchallengerhellcat"
 
 client_script {
     'vehicle_names.lua'    -- Not Required
